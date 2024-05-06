a72f974a619dc88ec9aed573a533cd42a16fb333
MoonJaeGyeong <bblbblan69@gmail.com>
2024-05-06 14:25:55 +0900

    build : 프로젝트 시작

---
+++ src/main/java/com/example/humorie/HumorieApplication.java
@@ -0,0 +1,13 @@
+package com.example.humorie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumorieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumorieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/humorie/HumorieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.humorie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumorieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
