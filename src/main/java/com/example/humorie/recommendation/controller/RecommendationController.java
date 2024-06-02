a7c4589c8d57620663c3b3553dc0fcf6702f91ea
MoonJaeGyeong <bblbblan69@gmail.com>
2024-06-02 22:39:40 +0900

    refactor : swagger Operation 추가

--- src/main/java/com/example/humorie/recommendation/controller/RecommendationController.java
+++ src/main/java/com/example/humorie/recommendation/controller/RecommendationController.java
@@ -4,6 +4,7 @@ import com.example.humorie.account.jwt.PrincipalDetails;
 import com.example.humorie.recommendation.dto.RecommendationCounselorDto;
 import com.example.humorie.recommendation.dto.RecommendationReviewDto;
 import com.example.humorie.recommendation.service.RecommendationService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,12 +24,14 @@ public class RecommendationController {
 
     private final RecommendationService recommendationService;
 
+    @Operation(summary = "상담사 추천 api 생성")
     @GetMapping("/counselor")
     public ResponseEntity<List<RecommendationCounselorDto>> recommendCounselor(@AuthenticationPrincipal PrincipalDetails principal) {
 
         return ResponseEntity.status(HttpStatus.OK).body(recommendationService.recommendCounselor(principal));
     }
 
+    @Operation(summary = "리뷰 추천 api 생성")
     @GetMapping("/review")
     public ResponseEntity<List<RecommendationReviewDto>> recommendReview(@AuthenticationPrincipal PrincipalDetails principal) {
 
