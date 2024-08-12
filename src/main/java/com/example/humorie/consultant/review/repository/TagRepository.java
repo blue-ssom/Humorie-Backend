e96babcb6edb4ff42b4aef295b891f7dea53a68f
hershyYJ <juj0478@naver.com>
2024-08-12 17:31:39 +0900

    feat: 태그 관련 기능 추가

--- src/main/java/com/example/humorie/consultant/review/repository/TagRepository.java
+++ src/main/java/com/example/humorie/consultant/review/repository/TagRepository.java
@@ -1,9 +1,22 @@
 package com.example.humorie.consultant.review.repository;
 
+import com.example.humorie.account.entity.AccountDetail;
 import com.example.humorie.consultant.review.entity.ReviewTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TagRepository extends JpaRepository<ReviewTag, Long> {
+
+    Optional<ReviewTag> findByTagNameAndAccount(String tagName, AccountDetail accountDetail);
+
+    List<ReviewTag> findByAccount(AccountDetail account);
+
+    Optional<ReviewTag> findByIdAndAccount(long tagId, AccountDetail account);
+
+    boolean existsByTagNameAndAccount(String tagName, AccountDetail account);
+
 }
