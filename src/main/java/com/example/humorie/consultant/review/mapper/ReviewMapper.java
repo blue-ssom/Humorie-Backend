d6c90b2748f0714a2e4b0254bf6cea5f6a7ea628
hershyYJ <juj0478@naver.com>
2024-08-12 17:39:08 +0900

    feat: 리뷰 관련 map struct 구현

---
+++ src/main/java/com/example/humorie/consultant/review/mapper/ReviewMapper.java
@@ -0,0 +1,30 @@
+package com.example.humorie.consultant.review.mapper;
+
+import com.example.humorie.consultant.review.dto.ReviewReq;
+import com.example.humorie.consultant.review.dto.ReviewRes;
+import com.example.humorie.consultant.review.entity.Review;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper
+public interface ReviewMapper {
+
+    ReviewMapper INSTANCE = Mappers.getMapper(ReviewMapper.class);
+
+    @Mapping(source = "id", target = "reviewId")
+    ReviewRes toReviewRes(Review review);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "symptom", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "account", ignore = true)
+    @Mapping(target = "counselor", ignore = true)
+    @Mapping(target = "tags", ignore = true)
+    Review toReview(ReviewReq reviewReq);
+
+    List<ReviewRes> toReviewResList(List<Review> reviews);
+
+}
