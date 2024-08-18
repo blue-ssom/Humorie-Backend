33e63d72aaf2b0cee553a578d1f85c3c0c4c34d6
MoonJaeGyeong <bblbblan69@gmail.com>
2024-08-18 16:30:10 +0900

    feat : 프론트 요청에 따른 reservation 생성 response 수정

--- src/main/java/com/example/humorie/reservation/dto/response/CreateReservationResDto.java
+++ src/main/java/com/example/humorie/reservation/dto/response/CreateReservationResDto.java
@@ -1,12 +1,16 @@
 package com.example.humorie.reservation.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class CreateReservationResDto {
     String reservationUid;
+    String counselorName;
+    Integer finalPrice;
+    String accountName;
+    String accountEmail;
+    // 가격, 예약자 이름, 예약자 이메일, 상담사 이름
 
-    public CreateReservationResDto(String reservationUid){
-        this.reservationUid = reservationUid;
-    }
 }
