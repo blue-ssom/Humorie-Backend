6ad9ff5b58d0f22744edbb255f056a40fb862ef2
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-08-18 17:08:39 +0900

    fix: 상담 내역 전체 조회 서비스에서 페이지 및 크기 파라미터 유효성 검사 추가

--- src/main/java/com/example/humorie/consult_detail/service/ConsultDetailService.java
+++ src/main/java/com/example/humorie/consult_detail/service/ConsultDetailService.java
@@ -51,7 +51,7 @@ public class ConsultDetailService {
     }
 
     // 상담 내역 전체 조회
-    public ConsultDetailPageDto findAllConsultDetail(PrincipalDetails principalDetails, Pageable pageable) {
+    public ConsultDetailPageDto findAllConsultDetail(PrincipalDetails principalDetails, Integer page, Integer size) {
         // PrincipalDetails 객체에서 AccountDetail 객체를 가져옴
         AccountDetail accountDetail = principalDetails.getAccountDetail();
         if (accountDetail == null) {
@@ -59,13 +59,33 @@ public class ConsultDetailService {
         }
         log.info("Account ID: " + accountDetail.getId());
 
+        // 파라미터 값이 없을 경우 예외 처리
+        if (page == null || size == null) {
+            throw new ErrorException(ErrorCode.REQUEST_ERROR);  // 잘못된 요청 예외 발생
+        }
+
+        // 페이지 번호 유효성 검사
+        if (page < 0) {
+            throw new ErrorException(ErrorCode.NEGATIVE_PAGE_NUMBER);
+        }
+
+        // 페이지 크기 유효성 검사
+        if (size < 1) {
+            throw new ErrorException(ErrorCode.NEGATIVE_PAGE_SIZE);
+        } else if (size > 9) {
+            throw new ErrorException(ErrorCode.INVALID_PAGE_SIZE);
+        }
+
+        // Pageable 객체 생성
+        Pageable pageable = PageRequest.of(page, size);
+
         // Page 객체를 가져옴
         Page<ConsultDetail> consultDetails = consultDetailRepository.findAllConsultDetail(accountDetail, pageable);
 
         // 총 페이지 수보다 요청된 페이지 번호가 클 경우 예외 처리
         if (pageable.getPageNumber() >= consultDetails.getTotalPages()) {
-            log.error("Page number {} exceeds total pages {}", pageable.getPageNumber(), consultDetails.getTotalPages());
-            throw new ErrorException(ErrorCode.REQUEST_ERROR);
+            log.error("Page number {} exceeds total pages {}", pageable.getPageNumber() + 1, consultDetails.getTotalPages());
+            throw new ErrorException(ErrorCode.INVALID_PAGE_NUMBER);
         }
 
         // 페이지가 비어 있는지 확인
