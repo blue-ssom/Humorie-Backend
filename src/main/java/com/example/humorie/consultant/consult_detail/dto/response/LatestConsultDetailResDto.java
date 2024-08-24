bbf0a049bfb7d605bc7a82319294c173bd477b9e
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-08-24 16:47:21 +0900

    feat : 상담사 식별자와 상담 방법 필드 추가

--- src/main/java/com/example/humorie/consultant/consult_detail/dto/response/LatestConsultDetailResDto.java
+++ src/main/java/com/example/humorie/consultant/consult_detail/dto/response/LatestConsultDetailResDto.java
@@ -9,19 +9,21 @@ import java.time.LocalDate;
 @Getter
 public class LatestConsultDetailResDto {
     private final Long id;
+    private final Long counselorId;
     private final String counselorName;
     private final double rating;
-    private final String location;
+    private final Boolean isOnline;
     private final LocalDate counselDate;
     private final String title;
     private final String content;
 
     @Builder
-    public LatestConsultDetailResDto(Long id, String counselorName, LocalDate counselDate, String status, String title, String symptom, String content, double rating, String location) {
+    public LatestConsultDetailResDto(Long id, Long counselorId, String counselorName, LocalDate counselDate, String title, String content, double rating, Boolean isOnline) {
         this.id = id;
+        this.counselorId = counselorId;
         this.counselorName = counselorName;
         this.rating = rating;
-        this.location = location;
+        this.isOnline = isOnline;
         this.counselDate = counselDate;
         this.title = title;
         this.content = content;
@@ -30,12 +32,13 @@ public class LatestConsultDetailResDto {
     public static LatestConsultDetailResDto fromEntity(ConsultDetail consultDetail) {
         return LatestConsultDetailResDto.builder()
                 .id(consultDetail.getId())
+                .counselorId(consultDetail.getCounselorId())
                 .counselorName(consultDetail.getCounselorName())
+                .rating(consultDetail.getRating())
+                .isOnline(consultDetail.getIsOnline())
                 .counselDate(consultDetail.getCounselDate())
                 .title(consultDetail.getTitle())
                 .content(consultDetail.getContent())
-                .rating(consultDetail.getRating())
-                .location(consultDetail.getLocation())
                 .build();
     }
 }
