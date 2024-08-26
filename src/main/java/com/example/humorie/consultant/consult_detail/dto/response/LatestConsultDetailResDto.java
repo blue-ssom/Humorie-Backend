7fb2415b435bdc142a18556fe2a1a74808cec95e
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-08-26 13:47:20 +0900

    fix : isOnline 필드 제거 및 JsonFormat 에너테이션 추가

--- src/main/java/com/example/humorie/consultant/consult_detail/dto/response/LatestConsultDetailResDto.java
+++ src/main/java/com/example/humorie/consultant/consult_detail/dto/response/LatestConsultDetailResDto.java
@@ -1,6 +1,7 @@
 package com.example.humorie.consultant.consult_detail.dto.response;
 
 import com.example.humorie.consultant.consult_detail.entity.ConsultDetail;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,18 +13,19 @@ public class LatestConsultDetailResDto {
     private final Long counselorId;
     private final String counselorName;
     private final double rating;
-    private final Boolean isOnline;
+    // 날짜 필드에 @JsonFormat 애너테이션 적용
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.M.d")
     private final LocalDate counselDate;
     private final String title;
     private final String content;
 
     @Builder
-    public LatestConsultDetailResDto(Long id, Long counselorId, String counselorName, LocalDate counselDate, String title, String content, double rating, Boolean isOnline) {
+    public LatestConsultDetailResDto(Long id, Long counselorId, String counselorName, LocalDate counselDate,
+                                     String title, String content, double rating) {
         this.id = id;
         this.counselorId = counselorId;
         this.counselorName = counselorName;
         this.rating = rating;
-        this.isOnline = isOnline;
         this.counselDate = counselDate;
         this.title = title;
         this.content = content;
@@ -35,7 +37,6 @@ public class LatestConsultDetailResDto {
                 .counselorId(consultDetail.getCounselorId())
                 .counselorName(consultDetail.getCounselorName())
                 .rating(consultDetail.getRating())
-                .isOnline(consultDetail.getIsOnline())
                 .counselDate(consultDetail.getCounselDate())
                 .title(consultDetail.getTitle())
                 .content(consultDetail.getContent())
