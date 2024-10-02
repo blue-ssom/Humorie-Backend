dd8758f6afb6f80b517c83b77822c90b946c0c5f
hershyYJ <juj0478@naver.com>
2024-10-02 16:41:14 +0900

    refactor: 클래스 이름 변경

---
+++ src/main/java/com/example/humorie/notice/dto/NoticeListDto.java
@@ -0,0 +1,29 @@
+package com.example.humorie.notice.dto;
+
+import com.example.humorie.notice.entity.Notice;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Builder
+public class NoticeListDto {
+    private Long id;
+    private String title;
+    private LocalDate createdDate;
+    private LocalTime createdTime;
+    private int viewCount;
+
+    // Notice 엔티티를 GetAllNoticeDto로 변환하는 메서드
+    public static NoticeListDto fromEntity(Notice notice) {
+        return NoticeListDto.builder()
+                .id(notice.getId())
+                .title(notice.getTitle())
+                .createdDate(notice.getCreatedDate())
+                .createdTime(notice.getCreatedTime())
+                .viewCount(notice.getViewCount())
+                .build();
+    }
+}
