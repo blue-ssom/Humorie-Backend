3d5a74066b00d3907a947ec8041b630011807856
hershyYJ <juj0478@naver.com>
2024-10-02 16:40:33 +0900

    refactor: 클래스 위치 변경

---
+++ src/main/java/com/example/humorie/mypage/dto/response/PointDto.java
@@ -0,0 +1,40 @@
+package com.example.humorie.mypage.dto.response;
+
+import com.example.humorie.mypage.entity.Point;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PointDto {
+
+    private long pointId;
+
+    private String title;
+
+    private int earnedPoints;
+
+    private int spentPoints;
+
+    private LocalDateTime transactionDate;
+
+    public static PointDto convertToPointDto(Point point) {
+        int earnedPoint = point.getType().equals("earn") ? point.getPoints() : 0;
+        int spentPoint = point.getType().equals("spend") ? point.getPoints() : 0;
+
+        return PointDto.builder()
+                .pointId(point.getId())
+                .title(point.getTitle())
+                .earnedPoints(earnedPoint)
+                .spentPoints(spentPoint)
+                .transactionDate(point.getTransactionDate())
+                .build();
+    }
+
+}
